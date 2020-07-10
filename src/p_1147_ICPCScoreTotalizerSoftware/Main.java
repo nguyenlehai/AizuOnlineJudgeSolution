2034d9f337f728b7f131ab56ba9e9f0edfce701c
HaiNguyen <nguyenlehai@hotmail.com>
2020-07-10 15:10:14 +0700

    Add solve of problems.

---
+++ src/p_1147_ICPCScoreTotalizerSoftware/Main.java
@@ -0,0 +1,29 @@
+package p_1147_ICPCScoreTotalizerSoftware;
+
+import java.util.Scanner;
+
+//https://onlinejudge.u-aizu.ac.jp/problems/1147
+public class Main {
+  public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	while (true) {
+
+	  int n = sc.nextInt();
+	  if (n == 0) {
+		break;
+	  }
+	  int min = 1001, max = 0, sum = 0;
+	  for (int i = 0; i < n; i++) {
+		int s = sc.nextInt();
+		if (min > s) {
+		  min = s;
+		}
+		if (max < s) {
+		  max = s;
+		}
+		sum += s;
+	  }
+	  System.out.println((sum - max - min) / (n - 2));
+	}
+  }
+}
---
+++ src/p_1179_Millennium/Main.java
@@ -0,0 +1,40 @@
+package p_1179_Millennium;
+
+import java.util.Scanner;
+
+//https://onlinejudge.u-aizu.ac.jp/problems/1147
+public class Main {
+
+  public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	final int DAY_OF_MILLENNIUM = 196470;
+	final int DAY_OF_SPECIAL_YEAR = 200;
+	final int DAY_OF_NORMAL_YEAR = 195;
+	final int DAY_OF_SMALL_DAY = 19;
+	final int DAY_OF_BIG_MONTH = 20;
+
+	int n = sc.nextInt();
+	for (int i = 0; i < n; i++) {
+	  int year = sc.nextInt();
+	  int month = sc.nextInt();
+	  int date = sc.nextInt();
+
+	  int specialYear = (year - 1) / 3;
+	  int normalYear = year - 1 - specialYear;
+	  int result = specialYear * DAY_OF_SPECIAL_YEAR;
+	  result += normalYear * DAY_OF_NORMAL_YEAR;
+
+	  if (year % 3 == 0) {
+		result += (month - 1) * DAY_OF_BIG_MONTH;
+	  } else {
+		int monthOfResult = (month - 1) / 2;
+		int dayOfResult = month - 1 - monthOfResult;
+		result += monthOfResult * DAY_OF_SMALL_DAY;
+		result += dayOfResult * DAY_OF_BIG_MONTH;
+	  }
+	  result += date - 1;
+	  result = DAY_OF_MILLENNIUM - result;
+	  System.out.println(result);
+	}
+  }
+}
